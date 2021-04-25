ec95980b5be82348076a77b21b47057e7bac9286
amogh <amogh.johri@iiitb.org>
2021-04-25 16:41:39 +0530

    added PACE data-set anaylsis

--- Kernelization/test.py
+++ Kernelization/test.py
@@ -1,5 +1,7 @@
 import os 
+import statistics
 import networkx as nx 
+import progressbar
 
 from util import drawGraph
 
@@ -28,6 +30,25 @@ def get_graph(code=5):
     return G
 
 if __name__ == "__main__":
-    G = get_graph()
-    print("|V|: ", len(G.nodes))
-    print("|E|: ", len(G.edges))
+    p_arr = []
+    bar = progressbar.ProgressBar(maxval=201, \
+    widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+    bar.start()
+    for i in range(1, 200, 2):
+        G = get_graph(code=i)
+        v = len(list(G.nodes))
+        e = len(list(G.edges))
+        p_arr.append(e/(v*v/2))
+        bar.update(i+2)
+    bar.finish()
+    print("Min: ", min(p_arr))
+    print("Max: ", max(p_arr))
+    print("Mean: ", statistics.mean(p_arr))
+    print("Median: ", statistics.median(p_arr))
+
+"""
+Min   :  2.381614667858874e-05
+Max   :  0.17610864197530865
+Mean  :  0.01887090304564615
+Median:  0.00023000940504558766
+"""
